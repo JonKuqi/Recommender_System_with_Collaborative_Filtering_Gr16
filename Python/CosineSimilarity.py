b5b12cdb5b1f4ecb33c86f4d4edfd59a15f651a0
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2024-12-19 00:44:10 +0100

    Chnages

--- Python/CosineSimilarity.py
+++ Python/CosineSimilarity.py
@@ -1,12 +1,16 @@
 
 class ItemBasedFilter:
 
+    #To return a dict of books and their score from 0 to 1 || 0 to 5
     @staticmethod
     def cosineSimilarity():
         import ctypes
 
         c = ctypes.CDLL('../CFiles/CosineSimilarity.dll')
         c.say_hello()
+
+
+
 
 
         pass
