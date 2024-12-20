c9e0d8220513bcc6a208d13f17ef8abf33d5254f
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2024-12-20 16:01:29 +0100

    Cosine Similarity For different categories. Added TfidVectorizer

--- CFiles/CosineSimilarity.c
+++ CFiles/CosineSimilarity.c
@@ -6,68 +6,67 @@
 #include <stdlib.h>    // For malloc
 
 
-__declspec(dllexport) double cosineSimilarity(const double *A, const double *B, int n){
+__declspec(dllexport) double cosineSimilarity(const double *A, const double *B, int n) {
 
     //    FORMULA
     //  dotProduct = addition from i to n  (Ai * Bi)
     //  sumASquared = addition from i to n (Ai)^2
     //  sumBSquared = addition from i to n (Bi)^2
     //
-    // cosine_similarity = dotProduct/ sqrt(sumASquared) * sqrt(sumBSquared)
+    // cosine_similarity = dotProduct / (sqrt(sumASquared) * sqrt(sumBSquared))
 
-    //Vector 256-bit for 4 double values
+    // 256-bit vectors for 4 double values
     __m256d vectorA;
     __m256d vectorB;
     __m256d resultVector;
 
-    //Accumulators
-    //  A*B = dotProduct
-    //  a1^2 + a2^2 +...+ an^2 = sumASquared
-    //  b1^2 + b2^2 +...+ bn^2 = sumBSquared
-
-    __m256d dotProduct = _mm256_setzero_pd(); //Make it zero initially
+    // Accumulators
+    __m256d dotProduct = _mm256_setzero_pd(); // Initialize to zero
     __m256d sumASquared = _mm256_setzero_pd();
     __m256d sumBSquared = _mm256_setzero_pd();
 
-    // 4 by 4 we calculate these values
-
     int i;
 
-    for(int i=0; i<= n-4; i+=4){
-        //Take 4 elements and put into the vectors
+    // Process 4 elements at a time
+    for (i = 0; i <= n - 4; i += 4) {
+        // Load 4 elements into the vectors
         vectorA = _mm256_loadu_pd(&A[i]);
         vectorB = _mm256_loadu_pd(&B[i]);
 
+        // Compute the dot product for these 4 elements
         resultVector = _mm256_mul_pd(vectorA, vectorB);
-        dotProduct = _mm256_add_pd(dotProduct, vectorB);
+        dotProduct = _mm256_add_pd(dotProduct, resultVector);
 
+        // Compute the squared sums for A and B
         resultVector = _mm256_mul_pd(vectorA, vectorA);
         sumASquared = _mm256_add_pd(sumASquared, resultVector);
 
         resultVector = _mm256_mul_pd(vectorB, vectorB);
         sumBSquared = _mm256_add_pd(sumBSquared, resultVector);
-
-
     }
 
-    //Convert to normal arrays
+    // Convert to normal arrays to sum the partial results
     double dotP[4], sumA[4], sumB[4];
     _mm256_storeu_pd(dotP, dotProduct);
     _mm256_storeu_pd(sumA, sumASquared);
     _mm256_storeu_pd(sumB, sumBSquared);
 
-    //Sum parts
+    // Sum up the parts
     double dotProductFinal = dotP[0] + dotP[1] + dotP[2] + dotP[3];
     double sumASquaredFinal = sumA[0] + sumA[1] + sumA[2] + sumA[3];
     double sumBSquaredFinal = sumB[0] + sumB[1] + sumB[2] + sumB[3];
 
-    //Remaining numbers that that were left because of the 4 partition
-    for(;i<n;i++){
+    // Handle the remaining elements that didn't fit into the 4-element partition
+    for (; i < n; i++) {
         dotProductFinal += A[i] * B[i];
         sumASquaredFinal += A[i] * A[i];
         sumBSquaredFinal += B[i] * B[i];
     }
 
+    // Prevent division by zero
+    if (sumASquaredFinal == 0 || sumBSquaredFinal == 0) {
+        return 0.0;
+    }
 
     return dotProductFinal / (sqrt(sumASquaredFinal) * sqrt(sumBSquaredFinal));
 }
@@ -124,5 +123,9 @@ __declspec(dllexport) double* weighted_sum(const double *array1, double weight1,
     }
 
     return resultArray;  // return the dynamically allocated result array
+}
+
+__declspec(dllexport) void free_array(double* array) {
+    free(array);
 }
 
--- Python/CosineSimilarity.py
+++ Python/CosineSimilarity.py
@@ -1,6 +1,12 @@
+import math
+
 from DataFetcher import DataFetcher
 import re
 from collections import defaultdict
+import ctypes
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
 
 class ItemBasedFilter:
 
@@ -12,6 +18,8 @@ class ItemBasedFilter:
 
     #To return a dict of books and their score from  0 to 5
 
+    cFunctions = ctypes.CDLL('../CFiles/CosineSimilarity.dll')
+
     ## MAIN METHOD
     @staticmethod
     def getRecommendations(userId):
@@ -22,12 +30,14 @@ class ItemBasedFilter:
         allUserBooks = DataFetcher.getUserBooks()
 
         ratedBooksIds = set()
+
+
         ratedBooksRatings = {}
 
         for i in range(len(allUserBooks)):
             if allUserBooks[i]['userId'] == str(userId):
                 ratedBooksIds.add(allUserBooks[i]['bookId'])
-                ratedBooksRatings['bookId'] = allUserBooks[i]['rating']
+                ratedBooksRatings[allUserBooks[i]['bookId']] = allUserBooks[i]['rating']
 
 
         unRatedBooks = {}
@@ -39,13 +49,123 @@ class ItemBasedFilter:
             else:
                 unRatedBooks[bookId] = allBooks[bookId]
 
-        ItemBasedFilter.createVectorCategory(allBooks, "title")
+        ItemBasedFilter.filterAndSendToC(allBooks, ratedBooksIds, ratedBooksRatings)
 
 
 
     @staticmethod
-    def createVectorCategory(allBooks, category):
-        matrix = ItemBasedFilter.createVectorCategoryMatrix(allBooks, category)
+    def filterAndSendToC(allBooks, ratedBooksId, ratedBookRatings):
+
+        titleCategoryMatrix = ItemBasedFilter.createVectorCategory(allBooks, "title", True)
+        authorCategoryMatrix = ItemBasedFilter.createVectorCategory(allBooks, "author", True)
+        genreCategoryMatrix = ItemBasedFilter.createVectorCategory(allBooks, "genres", False)
+        descriptionCategoryMatrix = ItemBasedFilter.createVectorUsingTfidfVectorizer(allBooks, "description")
+
+
+        titleRated = {}
+        titleUnrated = {}
+        ItemBasedFilter.filterRatedUnrated(titleCategoryMatrix, titleRated, titleUnrated, ratedBooksId)
+        titleCosineSimilarityMatrix = {}
+        ItemBasedFilter.sendToC(titleRated, titleUnrated, titleCosineSimilarityMatrix)
+        ItemBasedFilter.getRatingForCategory(titleCosineSimilarityMatrix, ratedBookRatings)
+        print(titleCosineSimilarityMatrix['23'])
+
+
+        authorRated = {}
+        authorUnRated = {}
+        ItemBasedFilter.filterRatedUnrated(authorCategoryMatrix, authorRated, authorUnRated, ratedBooksId)
+        authorCosineSimilarityMatrix = {}
+        ItemBasedFilter.sendToC(authorRated, authorUnRated, authorCosineSimilarityMatrix)
+
+        genresRated = {}
+        genresUnRated = {}
+        ItemBasedFilter.filterRatedUnrated(genreCategoryMatrix, genresRated, genresUnRated, ratedBooksId)
+        genreCosineSimilarityMatrix = {}
+        ItemBasedFilter.sendToC(genresRated, genresUnRated, genreCosineSimilarityMatrix)
+
+        descriptionRated = {}
+        descriptionUnrated = {}
+        ItemBasedFilter.filterRatedUnrated(descriptionCategoryMatrix, descriptionRated, descriptionUnrated, ratedBooksId)
+        descriptionCosineSimilarityMatrix = {}
+        ItemBasedFilter.sendToC(descriptionRated, descriptionUnrated, descriptionCosineSimilarityMatrix)
+
+
+
+
+    #ratedBooksRating {bookId: 0.0-5, ...}
+    @staticmethod
+    def getRatingForCategory(categoryMatrix, ratedBooksRatings):
+
+        unratedBookLists = {}
+        for bookId in categoryMatrix:
+            for unRatedBookId in categoryMatrix[bookId]:
+                if unRatedBookId in unratedBookLists:
+                    unratedBookLists[unRatedBookId].append(categoryMatrix[bookId][unRatedBookId])
+                else:
+                    unratedBookLists[unRatedBookId] = [categoryMatrix[bookId][unRatedBookId]]
+
+        #print(unratedBookLists)
+
+
+
+
+
+
+
+
+    @staticmethod
+    def sendToC(ratedMap, unRatedMap, resultMatrix):
+
+        ItemBasedFilter.cFunctions.cosineSimilarity.argtypes = [ctypes.POINTER(ctypes.c_double),
+                                                ctypes.POINTER(ctypes.c_double),
+                                                ctypes.c_int]
+        ItemBasedFilter.cFunctions.cosineSimilarity.restype = ctypes.c_double
+
+        ItemBasedFilter.cFunctions.free_array.argtypes = [ctypes.POINTER(ctypes.c_double)]
+        ItemBasedFilter.cFunctions.free_array.restype = None
+
+        # Helper function
+        def cosine_similarity_py(arrA, arrB):
+            n = len(arrA)
+            if len(arrB) != n:
+                raise ValueError("Input arrays must have the same length")
+
+            # Convert Python lists to ctypes arrays
+            arrayA = (ctypes.c_double * n)(*arrA)
+            arrayB = (ctypes.c_double * n)(*arrB)
+
+            # Call the C function
+            result = ItemBasedFilter.cFunctions.cosineSimilarity(arrayA, arrayB, n)
+            return result
+
+
+        for bookId in ratedMap:
+            for bookId_2 in unRatedMap:
+                resultMatrix.setdefault(bookId, {})
+                resultFromC = cosine_similarity_py(ratedMap[bookId], unRatedMap[bookId_2])
+                if  not math.isnan(resultFromC):
+                    resultMatrix[bookId][bookId_2] = resultFromC
+                else:
+                    resultMatrix[bookId][bookId_2] = 0
+
+
+
+    @staticmethod
+    def filterRatedUnrated(matrix, rated, unrated, ratedBooksRating):
+        for bookId in matrix:
+            if bookId in ratedBooksRating:
+                rated[bookId] = matrix[bookId]
+            else:
+                unrated[bookId] = matrix[bookId]
+
+
+    @staticmethod
+    def createVectorCategory(allBooks, category, sentence):
+        if sentence:
+            matrix = ItemBasedFilter.createVectorCategoryMatrixSentences(allBooks, category)
+        else:
+            matrix = ItemBasedFilter.createVectorCategoryMatrixLists(allBooks, category)
+
         vectorMatrix = defaultdict(list)
 
         for bookId in allBooks:
@@ -57,13 +177,39 @@ class ItemBasedFilter:
         return vectorMatrix
 
 
+
     @staticmethod
-    def createVectorCategoryMatrix(allBooks, category) -> dict:
+    def createVectorUsingTfidfVectorizer(allBooks, category):
+
+        categoryAll = {}
+
+        for bookId in allBooks:
+            categoryAll[bookId] = allBooks[bookId][category]
+
+
+        category_keys = list(categoryAll.keys())
+
+        vectorizer = TfidfVectorizer(stop_words='english')
+        categoryMatrixNoKeys = vectorizer.fit_transform(categoryAll.values())
+
+        toReturn = {}
+        for i, bookId in enumerate(category_keys):
+            toReturn[bookId] = [float(value) for value in categoryMatrixNoKeys[i].toarray()[0]]
+
+        # for bookId, vector in toReturn.items():
+        #     print(f"Book ID: {bookId}\nVector: {vector}\n")
+
+        return toReturn
+
+
+    @staticmethod
+    def createVectorCategoryMatrixSentences(allBooks, category) -> dict:
         returnMatrix = {}
 
         for bookId in allBooks:
 
             valueCategory = allBooks[bookId][category]
+
             words = [word.lower() for word in re.findall(r'\w+', valueCategory)]
 
             for word in words:
@@ -76,19 +222,59 @@ class ItemBasedFilter:
                     returnMatrix[word] = {bookId:1}
 
 
-        print(returnMatrix)
+        #print(returnMatrix)
+        return returnMatrix
+
+
+    @staticmethod
+    def createVectorCategoryMatrixLists(allBooks, category):
+        returnMatrix = {}
+
+        for bookId in allBooks:
+            words = allBooks[bookId][category]
+
+            for word in words:
+                if word in returnMatrix:
+                    if bookId in returnMatrix[word]:
+                        returnMatrix[word][bookId] += 1
+                    else:
+                        returnMatrix[word][bookId] = 1
+                else:
+                    returnMatrix[word] = {bookId: 1}
+
+        # print(returnMatrix)
         return returnMatrix
 
 
 
-    @staticmethod
-    def cosineSimilarity():
-        # import ctypes
-        #
-        # c = ctypes.CDLL('../CFiles/CosineSimilarity.dll')
-        # c.say_hello()
 
-        pass
+
+    #Just for testing purposes. NOT IN USE
+    @staticmethod
+    def cosineTest(arrA, arrB):
+        if len(arrA) != len(arrB):
+            raise ValueError("Input arrays must have the same length")
+
+        # Compute the dot product
+        dot_product = sum(a * b for a, b in zip(arrA, arrB))
+
+        # Compute the magnitudes
+        sum_a_squared = sum(a * a for a in arrA)
+        sum_b_squared = sum(b * b for b in arrB)
+
+        magnitude_a = math.sqrt(sum_a_squared)
+        magnitude_b = math.sqrt(sum_b_squared)
+
+        # Handle division by zero
+        if magnitude_a == 0 or magnitude_b == 0:
+            return 0.0
+
+        # Calculate cosine similarity
+        return dot_product / (magnitude_a * magnitude_b)
+
+
+
+
 
 ItemBasedFilter.getRecommendations(1)
 
